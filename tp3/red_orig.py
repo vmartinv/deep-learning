8fe715de22d636bb04ad71a25aba56de7206f093
Martin Villagra <mvillagra0@gmail.com>
2016-10-27 17:54:28 -0300

    red orig funcionando

--- tp3/red_orig.py
+++ tp3/red_orig.py
@@ -1,22 +1,16 @@
-'''Trains a simple convnet on the MNIST dataset.
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
 from __future__ import print_function
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 import os
 from keras.datasets import mnist
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
 from scipy import misc
 from time import time
-import matplotlib.pyplot as plt
+# ~ import matplotlib.pyplot as plt
 
 from keras.preprocessing.image import ImageDataGenerator
 import shutil
@@ -24,6 +18,8 @@ import shutil
 batch_size = 128
 nb_classes = 91
 nb_epoch = 12
+
+LOAD_MODEL = False
 
 # input image dimensions
 img_rows, img_cols = 32, 32
@@ -38,121 +34,96 @@ def prepare_show(face):
     m = face.min()
     M = face.max()
     return (face-m)/(M-m)
+
+imgDataGen = ImageDataGenerator(featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    zca_whitening=False,
+    rotation_range=0.,
+    width_shift_range=0.,
+    height_shift_range=0.,
+    shear_range=0.,
+    zoom_range=0.,
+    channel_shift_range=0.,
+    fill_mode='nearest',
+    cval=0.,
     
-# the data, shuffled and split between train and test sets
-def get_images(base, tam=None):
-    print("Cargando "+base+"...")
-    extension = '.png'
-    files = []
-    for clase in os.listdir(base):
-        inputdir = os.path.join(base, clase)
-        for fileName in os.listdir(inputdir):
-            if fileName.endswith(extension):
-                files.append((os.path.join(inputdir, fileName), clase))
-    np.random.shuffle(files)
-    if tam is not None:
-        files = files[:int(len(files)*tam)]
-    r = []
-    last=-1
-    print("Se van a cargar "+str(len(files))+" imagenes.")
-    for i,(fileName,clase) in enumerate(files):
-        if int((float(i)/len(files))*100.)!=last:
-            last=int((float(i)/len(files))*100.)
-            if last%10==0:
-                print(str(last)+"% Completado")
-        r.append((np.array(misc.imread(fileName)), clase))
-    return (np.array([img for img,_ in r]), np.array([int(c)-32 for _,c in r]))
- 
-(X_train, y_train) = get_images('dataset/train', 1.0)
-(X_valid, y_valid) = get_images('dataset/valid', 1.0)
-(X_test, y_test) = get_images('dataset/test', 1.0)
+    horizontal_flip=False,
+    vertical_flip=False,
+    rescale=1/255.,
+    dim_ordering="tf")
 
-if K.image_dim_ordering() == 'th':
-    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-    X_valid = X_valid.reshape(X_valid.shape[0], 1, img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+train_generator = imgDataGen.flow_from_directory("dataset/train", target_size=(img_rows, img_cols), color_mode='grayscale', batch_size=batch_size)
+test_generator = imgDataGen.flow_from_directory("dataset/test", target_size=(img_rows, img_cols), color_mode='grayscale',  batch_size=batch_size)
+valid_generator = imgDataGen.flow_from_directory("dataset/valid", target_size=(img_rows, img_cols), color_mode='grayscale',  batch_size=batch_size)
+
+input_shape = (img_rows, img_cols, 1)
+
+nombre_red = os.path.basename(__file__) + '-' + str(int(time()))
+
+if LOAD_MODEL:
+    print("Cargando modelo...")
+    model = load_model("red_orig.py-model-1477598524.h5")
 else:
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_valid = X_valid.reshape(X_valid.shape[0], img_rows, img_cols, 1)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    print("Armando modelo...")
+    model = Sequential()
 
-X_train = X_train.astype('float32')
-X_valid = X_valid.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_valid /= 255
-X_test /= 255
-print('X_train shape:', X_train.shape)
-print(X_train.shape[0], 'train samples')
-print(X_valid.shape[0], 'valid samples')
-print(X_test.shape[0], 'test samples')
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+                            border_mode='valid',
+                            input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(Dropout(0.25))
 
-# convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_valid = np_utils.to_categorical(y_valid, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
 
-model = Sequential()
+    print("Compilando...")
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adadelta',
+                  metrics=['accuracy'])
+       
+    print("Entrenando red: "+nombre_red)
+    history = model.fit_generator(train_generator, samples_per_epoch=269018, nb_epoch=nb_epoch,
+              verbose=1, validation_data=valid_generator, nb_val_samples=25000)
 
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                        border_mode='valid',
-                        input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(0.25))
+    file_name = nombre_red+'-model.h5'
+    print("Guardando pesos en "+file_name+"...")
+    model.save(file_name)
 
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(nb_classes))
-model.add(Activation('softmax'))
+# ~ # summarize history for accuracy
+# ~ plt.plot(history.history['acc'])
+# ~ plt.plot(history.history['val_acc'])
+# ~ plt.title(nombre_red+' model accuracy')
+# ~ plt.ylabel('accuracy')
+# ~ plt.xlabel('epoch')
+# ~ plt.legend(['train', 'val'], loc='upper right')
+# ~ plt.savefig(nombre_red+'-acc.png', bbox_inches='tight', dpi = 150)
+# ~ plt.clf()
+# ~ # summarize history for loss
+# ~ plt.plot(history.history['loss'])
+# ~ plt.plot(history.history['val_loss'])
+# ~ plt.title(nombre_red+' model loss')
+# ~ plt.ylabel('loss')
+# ~ plt.xlabel('epoch')
+# ~ plt.legend(['train', 'val'], loc='lower right')
+# ~ plt.savefig(nombre_red+'-loss.png', bbox_inches='tight', dpi = 150)
+# ~ plt.clf()
 
-print("Compilando...")
-model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
-              metrics=['accuracy'])
-   
-nombre_red = os.path.basename(__file__)
-print("Entrenando red: "+nombre_red)
-history = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-          verbose=1, validation_data=(X_valid, Y_valid))
-
-timestamp = str(int(time()))
-file_name = nombre_red+'-model-'+timestamp+'.h5'
-print("Guardando pesos en "+file_name+"...")
-model.save(file_name)
-
-# summarize history for accuracy
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title(nombre_red+' model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper right')
-plt.savefig(nombre_red+'-acc-'+timestamp+'.png', bbox_inches='tight', dpi = 150)
-plt.clf()
-# summarize history for loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title(nombre_red+' model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='lower right')
-plt.savefig(nombre_red+'-loss-'+timestamp+'.png', bbox_inches='tight', dpi = 150)
-plt.clf()
-
-
-score = model.evaluate(X_train, Y_train, verbose=0)
+print("Evaluando modelo...")
+score = model.evaluate_generator(train_generator, val_samples=50000)
 print('Train score:', score[0])
-print('Train accuracy:', score[1])  
-score = model.evaluate(X_valid, Y_valid, verbose=0)
+print('Train accuracy:', score[1])
+score = model.evaluate_generator(valid_generator, val_samples=50000)
 print('Valid score:', score[0])
 print('Valid accuracy:', score[1])
-score = model.evaluate(X_test, Y_test, verbose=0)
+score = model.evaluate_generator(test_generator, val_samples=50000)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
