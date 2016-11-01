e2149a317c418eb231bf7ff11af875a0cf1d8107
Martin Villagra <mvillagra0@gmail.com>
2016-11-01 02:36:19 -0300

    improve analytics

--- tp3/eval_model.py
+++ tp3/eval_model.py
@@ -5,55 +5,74 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import defaultdict
+import operator
 
-if len(argv)!=2:
-    print("Usage python eval_model.py <model.h5>")
+THRESHOLD = 0.2
+
+if len(argv)!=3:
+    print("Usage python eval_model.py <model.h5> <db.h5>")
     exit(1)
 
 model = load_model(argv[1])
+X, Y = base.H5Dataset(argv[2]).get_data()
+total = X.shape[0]
+cases = defaultdict(lambda: ([], []))
 
 acc={}
 cant={}
 
-data = base.H5Dataset("dataseth5/train.h5", "Todo el conjunto de training")
-total = data.get_data()[0].shape[0]
-acc['Todos'] = data.evaluate(model)[1]
+def get_acc(name, X, Y, verbose=0):
+    print("Evaluando {}...".format(name))
+    acc = model.evaluate(X, Y, verbose=verbose)[1]
+    print "\r"+" "*80+"\r",
+    print("Cantidad: {}".format(X.shape[0]))
+    print("Accuracy: {:.2f}".format(acc))
+    print
+    return acc
+
+def save_graph():
+    plt.rcParams["figure.figsize"] = [8, 12]
+    y_pos = np.arange(len(acc))
+    asdf = [(ch, ac, q) for (ch,ac),(_,q) in zip(acc.iteritems(), cant.iteritems())]
+    asdf = sorted(asdf, key=operator.itemgetter(2))
+    
+    plt.barh(y_pos, [ac for __, ac, _ in asdf], align='center', alpha=0.4)
+    plt.yticks(y_pos, [ch for ch, __, _ in asdf])
+    plt.xlabel('Accuracy')
+    plt.ylabel('Caracter')
+    plt.grid(True)
+    plt.title('')
+    graphfile=argv[1].replace('--model.h5', '--accbychar.png')
+    for i, (ch, a, q) in enumerate(asdf):
+        plt.text(a + 0.01, i, "%.2f%%"%(q/float(total)*100.), color='black', fontweight='bold')
+    plt.savefig(graphfile, bbox_inches='tight', dpi = 300)
+    plt.clf()
+    
+acc['Todos'] = get_acc('todo el dataset', X, Y, verbose=1)
 cant['Todos'] = total
 
-for ch in range(0, 91):
-    data = base.H5Dataset("dataseth5/train.h5", "Caracter " + chr(ch+32))
-    data.filter(lambda _, y: base.to_categorical(y)==ch)
-    name = "preview"+str(ch+32).zfill(3)
-    #~ print("Guardando vista previa en %s..."%(name))
-    #~ data.preview(name)
-    score = data.evaluate(model)
-    if score is not None and data.get_data()[0].shape[0]>1000:
-        acc[chr(ch+32)]=score[1]
-        cant[chr(ch+32)]=data.get_data()[0].shape[0]
+print('Separando dataset...')
+for x,y in zip(X, Y):
+    cases[base.to_categorical(y)][0].append(x)
+    cases[base.to_categorical(y)][1].append(y)
+cases = {ch:(np.array(X), np.array(Y)) for ch,(X,Y) in cases.iteritems() if len(X)/float(total)*1000>=THRESHOLD}
+
+cases = sorted(cases.items(), key=lambda (__, (_, Y)):Y.shape[0], reverse=True)
+for i, (nch, (X, Y)) in enumerate(cases):
+    ch = chr(nch+32)
+    print "({:.0f}%)".format(i/float(len(cases))*100.),
+    cacc = get_acc('caracter {}'.format(ch), X, Y)
+    acc[ch]=cacc
+    cant[ch]=X.shape[0]
         
     if ' ' in acc:
         acc['Espacios'] = acc[' ']
         cant['Espacios'] = cant[' ']
         del acc[' ']
         del cant[' ']
+    
+save_graph()
 
-    y_pos = np.arange(len(acc))
-    plt.barh(y_pos, acc.values(), align='center', alpha=0.4)
-    plt.yticks(y_pos, acc.keys())
-    plt.xlabel('Accuracy')
-    plt.ylabel('Caracter')
-    plt.grid(True)
-    plt.title('Accuracy por caracter')
-    graphfile=argv[1].replace('--model.h5', '--accbychar.png')
-    for i, (a, q) in enumerate(zip(acc.values(), cant.values())):
-        plt.text(a + 0.01, i, "%.2f%%"%(q/float(total)*100.), color='black', fontweight='bold')
-    plt.savefig(graphfile, bbox_inches='tight', dpi = 300)
-    plt.clf()
-
-    
-    
-    
-    plt.clf()
-    
 print(acc)
 
---
+++ tp3/h5_analyzer.py
@@ -0,0 +1,53 @@
+import base
+from sys import argv
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import operator
+from collections import defaultdict
+
+THRESHOLD = 0.4
+GRAPHFILE='per-char-analysis.png'
+
+if len(argv)!=2:
+    print("Usage python h5_analyzer.py <db.h5>")
+    exit(1)
+
+X, Y = base.H5Dataset(argv[1], "Conjunto").get_data()
+total = X.shape[0]
+
+cant = defaultdict(int)
+for y in Y:
+    cant[chr(base.to_categorical(y)+32)] += 1
+    
+cant['Espacios']=cant[' ']
+del cant[' ']
+
+cant = sorted(cant.items(), key=operator.itemgetter(1))
+for c,q in cant:
+    print("{}: {} ({:.2f}%)".format(c, q, q/float(total)*100.))
+print('Total: {}'.format(total))
+qotros = sum([q for _,q in cant if q/float(total)*100<THRESHOLD])
+cant = [(c,q) for c,q in cant if q/float(total)*100>=THRESHOLD]
+cant = sorted(cant+[('Otros', qotros)], key=operator.itemgetter(1))
+
+cant = list(reversed(cant))
+cant = [(c,q/float(total)*100.) for c,q in cant]
+C, Q = zip(*cant)
+
+#~ print(plt.rcParams["figure.figsize"])
+#~ plt.rcParams["figure.figsize"] = [8, 15]
+
+y_pos = np.arange(len(cant))
+plt.barh(y_pos, Q, align='center', alpha=0.4)
+plt.yticks(y_pos, C)
+plt.xlabel('Porcentaje del dataset que representa')
+plt.ylabel('Caracter')
+plt.grid(True)
+plt.title('')
+for i, q in enumerate(Q):
+    plt.text(q + 0.01, i, "{:.2f}%".format(q), color='black', fontweight='bold')
+plt.savefig(GRAPHFILE, bbox_inches='tight', dpi = 300, figsize=(100, 100))
+plt.clf()
+
